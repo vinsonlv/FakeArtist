48a7f95b668744a7c21acd5b260ac04510fd649b
vinsonlv <lvzhuoer@gmail.com>
2017-11-09 00:40:35 -0800

    make ios work

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -3,35 +3,46 @@
     'use strict';
 
     var svg = document.querySelector('svg'),
-        label = svg.querySelector('text'),
-        path = null,
-        coords = null;
+    label = svg.querySelector('text'),
+    path = null,
+    coords = null;
 
     function handleDrawMove(e) {
 
         e.preventDefault();
 
-        if (e.touches) { e = e.touches[0]; }
+        if (e.touches) {
+            e = e.touches[0];
+        }
 
         var rect = document.getElementById('sketch').getBoundingClientRect();
-        coords += 'L' + (e.pageX-rect.left) + ' ' + (e.pageY-rect.top);
+
+        if (e.pageY < rect.top) {
+            return;
+        }
+
+        coords += 'L' + (e.pageX - rect.left) + ' ' + (e.pageY - rect.top);
 
         path.setAttribute('d', coords);
 
     }
 
     function handleDrawStart(e) {
+        event = e;
+        if (e.touches) {
+            e = e.touches[0];
+        }
 
-        e.preventDefault();
+        var rect = document.getElementById('sketch').getBoundingClientRect();
 
-        if (e.touches) { e = e.touches[0]; }
+        if (e.pageY < rect.top) {
+            return;
+        }
+        event.preventDefault();
 
         if (label) {
-
             svg.removeChild(label);
-
             label = null;
-
         }
 
         path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
@@ -39,7 +50,7 @@
         svg.appendChild(path);
 
         var rect = document.getElementById('sketch').getBoundingClientRect();
-        coords = 'M' + (e.pageX-rect.left) + ' ' + (e.pageY-rect.top);
+        coords = 'M' + (e.pageX - rect.left) + ' ' + (e.pageY - rect.top);
 
         path.setAttribute('d', coords);
         path.setAttribute('fill', 'none');
@@ -47,7 +58,8 @@
         path.setAttribute('stroke-width', 2);
 
         svg.addEventListener('mousemove', handleDrawMove);
-        svg.addEventListener('touchmove', handleDrawMove);
+        //svg.addEventListener('touchmove', handleDrawMove);
+        document.addEventListener('touchmove', handleDrawMove);
 
     }
 
@@ -57,14 +69,18 @@
         coords = null;
 
         svg.removeEventListener('mousemove', handleDrawMove);
-        svg.removeEventListener('touchmove', handleDrawMove);
+        //svg.removeEventListener('touchmove', handleDrawMove);
+        document.removeEventListener('touchmove', handleDrawMove);
 
     }
 
     svg.addEventListener('mousedown', handleDrawStart);
-    svg.addEventListener('touchstart', handleDrawStart);
-    
-    svg.addEventListener('mouseup', handleDrawEnd);
-    svg.addEventListener('touchend', handleDrawEnd);
+    //svg.addEventListener('touchstart', handleDrawStart);
+    document.addEventListener('touchstart', handleDrawStart);
 
-}());
+    svg.addEventListener('mouseup', handleDrawEnd);
+    //svg.addEventListener('touchend', handleDrawEnd);
+    document.addEventListener('touchend', handleDrawEnd);
+
+}
+    ());
