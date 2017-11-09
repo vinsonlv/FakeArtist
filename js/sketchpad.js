2fca5174040066a98292980498cd02478068db3e
vinsonlv <lvzhuoer@gmail.com>
2017-11-08 23:57:42 -0800

    make android pad work

---
+++ js/sketchpad.js
@@ -0,0 +1,70 @@
+(function () {
+
+    'use strict';
+
+    var svg = document.querySelector('svg'),
+        label = svg.querySelector('text'),
+        path = null,
+        coords = null;
+
+    function handleDrawMove(e) {
+
+        e.preventDefault();
+
+        if (e.touches) { e = e.touches[0]; }
+
+        var rect = document.getElementById('sketch').getBoundingClientRect();
+        coords += 'L' + (e.pageX-rect.left) + ' ' + (e.pageY-rect.top);
+
+        path.setAttribute('d', coords);
+
+    }
+
+    function handleDrawStart(e) {
+
+        e.preventDefault();
+
+        if (e.touches) { e = e.touches[0]; }
+
+        if (label) {
+
+            svg.removeChild(label);
+
+            label = null;
+
+        }
+
+        path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+
+        svg.appendChild(path);
+
+        var rect = document.getElementById('sketch').getBoundingClientRect();
+        coords = 'M' + (e.pageX-rect.left) + ' ' + (e.pageY-rect.top);
+
+        path.setAttribute('d', coords);
+        path.setAttribute('fill', 'none');
+        path.setAttribute('stroke', localStorage.currentColor);
+        path.setAttribute('stroke-width', 2);
+
+        svg.addEventListener('mousemove', handleDrawMove);
+        svg.addEventListener('touchmove', handleDrawMove);
+
+    }
+
+    function handleDrawEnd() {
+
+        path = null;
+        coords = null;
+
+        svg.removeEventListener('mousemove', handleDrawMove);
+        svg.removeEventListener('touchmove', handleDrawMove);
+
+    }
+
+    svg.addEventListener('mousedown', handleDrawStart);
+    svg.addEventListener('touchstart', handleDrawStart);
+    
+    svg.addEventListener('mouseup', handleDrawEnd);
+    svg.addEventListener('touchend', handleDrawEnd);
+
+}());
